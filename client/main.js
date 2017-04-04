75507f8b779a06f13d7c7a556ffc70b6a936ae38
George Higbie <gehigbie@gmail.com>
2017-04-03 17:31:14 -0700

    Add default data to be passed

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 //import "./../imports/utils";
-import {greetUser, name} from "./../imports/utils";
+import someDefault, {greetUser, name} from "./../imports/utils";
 import {add} from "./../imports/math";
 
 console.log("Log from /client/main.js");
 console.log(greetUser());
 console.log(name);
 console.log(add(7+7));
+console.log(someDefault);
--- imports/utils.js
+++ imports/utils.js
@@ -3,3 +3,4 @@ console.log("Log from /imports/utils.js");
 export let greetUser = () => "Hello User";
 export let name = "George Higbie";
 
+export default "Default val";
