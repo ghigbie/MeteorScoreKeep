0a4c0387e1b4b2e54c410b1716b949bf994c7659
George Higbie <gehigbie@gmail.com>
2017-04-05 16:12:45 -0700

    Add array of players to jsx

--- client/main.js
+++ client/main.js
@@ -2,6 +2,20 @@ import React from "react"; //to import from  an npm module all you need to do sp
 import ReactDOM from "react-dom";
 import {Meteor} from "meteor/meteor"; //meteor imports are imported with "metoer/[*** some meteor module name ****]"
 
+const players = [{
+	_id: 1,
+	name: Lauren,
+	score: 99
+},{
+	_id: 2,
+	name: Cody,
+	score: -1
+}, {
+	_id: 3,
+	name: Mike,
+	score: -50
+}];
+
 Meteor.startup( () => {
 	let title = "Score Keep";
 	let name = "Mike";
